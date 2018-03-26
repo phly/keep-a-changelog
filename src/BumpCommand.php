873620b9302838eb092f72197df9da78fcbb10c1
Matthew Weier O'Phinney <matthew@zend.com>
2018-03-25 20:57:33 -0500

    Merge all bump commands into one
    Merges all bump commands into one; a required constructor argument now
    governs how it behaves.

---
+++ src/BumpCommand.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog-tagger for the canonical source repository
+ * @copyright Copyright (c) 2018 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog-tagger/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Phly\KeepAChangelog;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Bump a changelog version.
+ *
+ * Bumps a changelog to the next bugfix, minor, or major version, based
+ * on how the command is configured at initialization.
+ */
+class BumpCommand extends Command
+{
+    public const BUMP_BUGFIX = 'bugfix';
+    public const BUMP_MAJOR = 'major';
+    public const BUMP_MINOR = 'minor';
+
+    private const DESC_TEMPLATE = 'Create a new changelog entry for the next %s release.';
+
+    private const HELP_TEMPLATE = <<< 'EOH'
+Add a new %1$s release entry to the changelog, based on the latest release.
+
+Parses the CHANGELOG.md file to determine the latest release, and creates
+a new entry representing the next %1$s release.
+
+EOH;
+
+    /** @var string[] */
+    private $bumpMethods = [
+        self::BUMP_BUGFIX => 'bumpBugfixVersion',
+        self::BUMP_MAJOR => 'bumpMajorVersion',
+        self::BUMP_MINOR => 'bumpMinorVersion',
+    ];
+
+    /** @var string */
+    private $type;
+
+    public function __construct(string $type, string $name = null)
+    {
+        if (! in_array($type, array_keys($this->bumpMethods), true)) {
+            throw new Exception\InvalidBumpTypeException();
+        }
+
+        $this->type = $type;
+        parent::__construct($name);
+    }
+
+    protected function configure() : void
+    {
+        $this->setDescription(sprintf(
+            self::DESC_TEMPLATE,
+            $this->type
+        ));
+
+
+        $this->setHelp(sprintf(
+            self::HELP_TEMPLATE,
+            $this->type
+        ));
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) : int
+    {
+        $cwd = realpath(getcwd());
+
+        $changelogFile = sprintf('%s/CHANGELOG.md', $cwd);
+        if (! is_readable($changelogFile)) {
+            throw new Exception\ChangelogFileNotFoundException();
+        }
+
+        $method = $this->bumpMethods[$this->type];
+
+        $bumper = new ChangelogBump($changelogFile);
+        $latest = $bumper->findLatestVersion();
+        $version = $bumper->$method($latest);
+        $bumper->updateChangelog($version);
+
+        $output->writeln(sprintf(
+            '<info>Bumped changelog version to %s</info>',
+            $version
+        ));
+
+        return 0;
+    }
+}
