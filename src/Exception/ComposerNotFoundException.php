8b1a2fad63c2f71d11bb3425682575fea44dd348
Matthew Weier O'Phinney <matthew@zend.com>
2018-04-11 16:48:09 -0500

    Adds tests for ComposerPackage class

---
+++ src/Exception/ComposerNotFoundException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog-tagger for the canonical source repository
+ * @copyright Copyright (c) 2018 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog-tagger/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Phly\KeepAChangelog\Exception;
+
+use RuntimeException;
+
+class ComposerNotFoundException extends RuntimeException
+{
+    public static function at(string $path) : self
+    {
+        return new self(sprintf(
+            'Unable to find composer.json file at %s; cannot detect package name',
+            $path
+        ));
+    }
+}
