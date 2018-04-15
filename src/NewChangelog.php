14f88efafb73d586e6a680682c6332f493044a49
Matthew Weier O'Phinney <matthew@zend.com>
2018-04-15 14:52:56 -0500

    Adds command for creating a new CHANGELOG.md file
    Syntax:
    
    ```bash
    $ keep-a-changelog new [--file|-f] [--initial-version|-i]
    ```
    
    Creates a new changelog file, defaulting to CHANGELOG.md in the current
    directory if no --file option is provided. The initial version is 0.1.0
    unless --initial-version is provided by the user.

---
+++ src/NewChangelog.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog-tagger for the canonical source repository
+ * @copyright Copyright (c) 2018 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog-tagger/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Phly\KeepAChangelog;
+
+/**
+ * Create a new changelog file.
+ */
+class NewChangelog
+{
+    private const TEMPLATE = <<< 'EOT'
+# Changelog
+
+All notable changes to this project will be documented in this file, in reverse chronological order by release.
+
+## %s - TBD
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- Nothing.
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- Nothing.
+
+### Fixed
+
+- Nothing.
+
+EOT;
+
+    public function __invoke(string $file, string $version) : void
+    {
+        $contents = sprintf(self::TEMPLATE, $version);
+        file_put_contents($file, $contents);
+    }
+}
