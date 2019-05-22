e3300b722f5e84f7de054fbf5dc9ad0fff4b208f
Matthew Weier O'Phinney <matthew@zend.com>
2019-05-22 11:23:55 -0500

    fix: remove constructor args from Release\ConfigListener
    Since the dispatcher is expected to be passed via the event itself, we
    no longer need to accept it via the constructor.

--- src/Release/ConfigListener.php
+++ src/Release/ConfigListener.php
@@ -10,22 +10,20 @@ declare(strict_types=1);
 namespace Phly\KeepAChangelog\Release;
 
 use Phly\KeepAChangelog\Config\ConfigListener as BaseConfigListener;
-use Psr\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Marshal configuration for the ReleaseEvent
  *
- * Overrides the parent constructor to hardcode the flags for requring the
+ * Overrides the parent constructor to hardcode the flags for requiring the
  * package name and remote name.
  */
 class ConfigListener extends BaseConfigListener
 {
-    public function __construct(?EventDispatcherInterface $dispatcher = null)
+    public function __construct()
     {
         parent::__construct(
             $requiresPackageName = true,
             $requiresRemoteName = true,
-            $dispatcher
         );
     }
 }
