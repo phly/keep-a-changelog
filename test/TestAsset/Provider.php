7c704393eb29349c22bcc544fcbff6cc0875a5e3
Matthew Weier O'Phinney <matthew@zend.com>
2019-06-03 16:32:55 -0500

    qa: adds tests for ProviderSpec

---
+++ test/TestAsset/Provider.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog for the canonical source repository
+ * @copyright Copyright (c) 2019 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace PhlyTest\KeepAChangelog\TestAsset;
+
+use Phly\KeepAChangelog\Provider\ProviderInterface;
+
+class Provider implements ProviderInterface
+{
+    /** @param null|string */
+    public $domain;
+
+    /** @param null|string */
+    public $package;
+
+    /** @param null|string */
+    public $token;
+
+    /** @param null|string */
+    public $url;
+
+    public function canCreateRelease() : bool
+    {
+        return false;
+    }
+
+    public function canGenerateLinks() : bool
+    {
+        return false;
+    }
+
+    public function createRelease(
+        string $releaseName,
+        string $tagName,
+        string $changelog
+    ) : ?string {
+        return null;
+    }
+
+    public function generateIssueLink(int $issueIdentifier) : string
+    {
+        return '';
+    }
+
+    public function generatePatchLink(int $patchIdentifier) : string
+    {
+        return '';
+    }
+
+    public function domain() : string
+    {
+        return (string) $this->domain;
+    }
+
+    public function setPackageName(string $package) : void
+    {
+        $this->package = $package;
+    }
+
+    public function setToken(string $token) : void
+    {
+        $this->token = $token;
+    }
+
+    public function setUrl(string $url) : void
+    {
+        $this->url = $this->domain = $url;
+    }
+}
