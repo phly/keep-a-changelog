3910c701d1206c3e5c53c479ad8de094ec640b24
Matthew Weier O'Phinney <matthew@zend.com>
2019-06-04 12:16:12 -0500

    qa: adds tests for ShowVersionEvent

---
+++ test/Version/ShowVersionEventTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog for the canonical source repository
+ * @copyright Copyright (c) 2019 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Phly\KeepAChangelog\Version;
+
+use Phly\KeepAChangelog\Common\ChangelogEntryAwareEventInterface;
+use Phly\KeepAChangelog\Common\EventInterface;
+use Phly\KeepAChangelog\Version\ShowVersionEvent;
+use PHPUnit\Framework\TestCase;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ShowVersionEventTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->input      = $this->prophesize(InputInterface::class);
+        $this->output     = $this->prophesize(OutputInterface::class);
+        $this->dispatcher = $this->prophesize(EventDispatcherInterface::class);
+    }
+
+    public function createEvent(?string $version = null) : ShowVersionEvent
+    {
+        return new ShowVersionEvent(
+            $this->input->reveal(),
+            $this->output->reveal(),
+            $this->dispatcher->reveal(),
+            $version
+        );
+    }
+
+    public function testEventImplementsPackageEvent() : ShowVersionEvent
+    {
+        $event = $this->createEvent();
+        $this->assertInstanceOf(EventInterface::class, $event);
+        return $event;
+    }
+
+    /**
+     * @depends testEventImplementsPackageEvent
+     */
+    public function testEventImplementsChangelogAwareEvent(ShowVersionEvent $event)
+    {
+        $this->assertInstanceOf(ChangelogEntryAwareEventInterface::class, $event);
+    }
+
+    /**
+     * @depends testEventImplementsPackageEvent
+     */
+    public function testPropagationIsNotStoppedByDefault(ShowVersionEvent $event)
+    {
+        $this->assertFalse($event->isPropagationStopped());
+    }
+
+    /**
+     * @depends testEventImplementsPackageEvent
+     */
+    public function testIsNotFailedByDefault(ShowVersionEvent $event)
+    {
+        $this->assertFalse($event->failed());
+    }
+}
