1d763c70fa815b2feac6c922a8abe6fa4694ccbf
Matthew Weier O'Phinney <matthew@zend.com>
2019-06-05 15:52:50 -0500

    feat: creates EditChangelogLinksEvent
    Creates an event for managing the workflow of editing the links portion
    of a changelog file.

---
+++ src/Changelog/EditChangelogLinksEvent.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog for the canonical source repository
+ * @copyright Copyright (c) 2019 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Phly\KeepAChangelog\Changelog;
+
+use Phly\KeepAChangelog\Common\AbstractEvent;
+use Phly\KeepAChangelog\Common\ChangelogEntry;
+use Phly\KeepAChangelog\Common\EditorAwareEventInterface;
+use Phly\KeepAChangelog\Common\EditorProviderTrait;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use function sprintf;
+
+class EditChangelogLinksEvent extends AbstractEvent implements EditorAwareEventInterface
+{
+    use EditorProviderTrait;
+
+    /**
+     * If no links are discovered in the changelog file, we will be appending
+     * them to any existing content, instead of splicing them in.
+     *
+     * @var bool
+     */
+    private $appendLinksToChangelogFile = false;
+
+    /**
+     * Value object representing the discovered links from the changelog file.
+     *
+     * @var null|ChangelogEntry
+     */
+    private $links;
+
+    public function __construct(
+        InputInterface $input,
+        OutputInterface $output,
+        EventDispatcherInterface $dispatcher
+    ) {
+        $this->input      = $input;
+        $this->output     = $output;
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function isPropagationStopped() : bool
+    {
+        return $this->failed;
+    }
+
+    public function appendLinksToChangelogFile() : bool
+    {
+        return $this->appendLinksToChangelogFile;
+    }
+
+    public function links() : ?ChangelogEntry
+    {
+        return $this->links;
+    }
+
+    public function discoveredLinks(ChangelogEntry $links) : void
+    {
+        $this->links = $links;
+    }
+
+    public function noLinksDiscovered() : void
+    {
+        $this->appendLinksToChangelogFile = true;
+    }
+
+    public function editComplete(string $changelogFile) : void
+    {
+        $this->output->writeln(sprintf(
+            '<info>Completed editing links for file %s</info>',
+            $changelogFile
+        ));
+    }
+
+    public function editFailed(string $changelogFile) : void
+    {
+        $this->failed = true;
+        $this->output->writeln(sprintf(
+            '<error>Editing links for file %s failed</error>',
+            $changelogFile
+        ));
+        $this->output->writeln('Review the output above for potential errors.');
+    }
+}
---
+++ test/Changelog/EditChangelogLinksEventTest.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog for the canonical source repository
+ * @copyright Copyright (c) 2019 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace PhlyTest\KeepAChangelog\Changelog;
+
+use Phly\KeepAChangelog\Changelog\EditChangelogLinksEvent;
+use Phly\KeepAChangelog\Common\ChangelogEntry;
+use Phly\KeepAChangelog\Common\EditorAwareEventInterface;
+use Phly\KeepAChangelog\Common\EventInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EditChangelogLinksEventTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->input      = $this->prophesize(InputInterface::class);
+        $this->output     = $this->prophesize(OutputInterface::class);
+        $this->dispatcher = $this->prophesize(EventDispatcherInterface::class);
+
+        $this->output->writeln(Argument::any())->willReturn(null);
+    }
+
+    public function createEvent() : EditChangelogLinksEvent
+    {
+        return new EditChangelogLinksEvent(
+            $this->input->reveal(),
+            $this->output->reveal(),
+            $this->dispatcher->reveal()
+        );
+    }
+
+    public function testIsAPackageEvent() : EditChangelogLinksEvent
+    {
+        $event = $this->createEvent();
+        $this->assertInstanceOf(EventInterface::class, $event);
+        return $event;
+    }
+
+    /**
+     * @depends testIsAPackageEvent
+     */
+    public function testIsAnEditorAwareEvent(EditChangelogLinksEvent $event)
+    {
+        $this->assertInstanceOf(EditorAwareEventInterface::class, $event);
+    }
+
+    /**
+     * @depends testIsAPackageEvent
+     */
+    public function testPropagationIsNotStoppedByDefault(EditChangelogLinksEvent $event)
+    {
+        $this->assertFalse($event->isPropagationStopped());
+    }
+
+    /**
+     * @depends testIsAPackageEvent
+     */
+    public function testIsNotInFailureStateByDefault(EditChangelogLinksEvent $event)
+    {
+        $this->assertFalse($event->failed());
+    }
+
+    /**
+     * @depends testIsAPackageEvent
+     */
+    public function testDoesNotAppendLinksByDefault(EditChangelogLinksEvent $event)
+    {
+        $this->assertFalse($event->appendLinksToChangelogFile());
+    }
+
+    /**
+     * @depends testIsAPackageEvent
+     */
+    public function testDoesNotComposeLinksByDefault(EditChangelogLinksEvent $event)
+    {
+        $this->assertNull($event->links());
+    }
+
+    public function testDiscoveringLinksMakesThemAccessible()
+    {
+        $links = new ChangelogEntry();
+        $event = $this->createEvent();
+
+        $this->assertNull($event->discoveredLinks($links));
+        $this->assertSame($links, $event->links());
+    }
+
+    public function testNotifyingNoLinksDiscoveredTogglesAppendFlag()
+    {
+        $event = $this->createEvent();
+
+        $this->assertNull($event->noLinksDiscovered());
+        $this->assertTrue($event->appendLinksToChangelogFile());
+    }
+
+    public function testNotifyingEditCompleteEmitsOutputWithoutStoppingPropagationOrFailure()
+    {
+        $event = $this->createEvent();
+
+        $this->assertNull($event->editComplete('changelog.txt'));
+
+        $this->output
+            ->writeln(Argument::containingString('Completed editing links for file changelog.txt'))
+            ->shouldHaveBeenCalled();
+        $this->assertFalse($event->isPropagationStopped());
+        $this->assertFalse($event->failed());
+    }
+
+    public function testNotifyingEditFailedEmitsOutputAndStopsPropagationWithFailure()
+    {
+        $event = $this->createEvent();
+
+        $this->assertNull($event->editFailed('changelog.txt'));
+
+        $this->output
+            ->writeln(Argument::containingString('Editing links for file changelog.txt failed'))
+            ->shouldHaveBeenCalled();
+        $this->assertTrue($event->isPropagationStopped());
+        $this->assertTrue($event->failed());
+    }
+}
