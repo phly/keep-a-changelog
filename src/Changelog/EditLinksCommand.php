c648fe0f44b589ce75ee7f2126df1d8ccf699039
Matthew Weier O'Phinney <matthew@zend.com>
2019-06-06 10:03:45 -0500

    feat: adds EditLinksCommand: `keep-a-changelog changelog:edit-links`

---
+++ src/Changelog/EditLinksCommand.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog for the canonical source repository
+ * @copyright Copyright (c) 2018-2019 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Phly\KeepAChangelog\Changelog;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EditLinksCommand extends Command
+{
+    private const DESCRIPTION = 'Edit the links section of a changelog file.';
+
+    private const HELP = <<<'EOH'
+Changelog files can optionally have a section of links at the end of the file
+in the following format:
+
+    [link name]: <url>
+
+(Indentation is for documentation purposes; omit it in actual files.)
+
+This command will spawn your $EDITOR with any discovered links, and then ensure
+they are written back to the file on completion of any edits.
+EOH;
+
+    /** @var EventDispatcherInterface */
+    private $dispatcher;
+
+    public function __construct(EventDispatcherInterface $dispatcher, ?string $name = null)
+    {
+        $this->dispatcher = $dispatcher;
+        parent::__construct($name);
+    }
+
+    protected function configure() : void
+    {
+        $this->setDescription(self::DESCRIPTION);
+        $this->setHelp(self::HELP);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) : int
+    {
+        return $this->dispatcher
+                ->dispatch(new EditChangelogLinksEvent(
+                    $input,
+                    $output,
+                    $this->dispatcher
+                ))
+                ->failed()
+                    ? 1
+                    : 0;
+    }
+}
---
+++ test/Changelog/EditLinksCommandTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @see       https://github.com/phly/keep-a-changelog for the canonical source repository
+ * @copyright Copyright (c) 2019 Matthew Weier O'Phinney
+ * @license   https://github.com/phly/keep-a-changelog/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace PhlyTest\KeepAChangelog\Changelog;
+
+use Phly\KeepAChangelog\Changelog\EditChangelogLinksEvent;
+use Phly\KeepAChangelog\Changelog\EditLinksCommand;
+use PhlyTest\KeepAChangelog\ExecuteCommandTrait;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EditLinksCommandTest extends TestCase
+{
+    use ExecuteCommandTrait;
+
+    public function setUp()
+    {
+        $this->input      = $this->prophesize(InputInterface::class);
+        $this->output     = $this->prophesize(OutputInterface::class);
+        $this->dispatcher = $this->prophesize(EventDispatcherInterface::class);
+    }
+
+    public function statuses() : iterable
+    {
+        yield 'failure' => [$failed = true, 1];
+        yield 'success' => [$failed = false, 0];
+    }
+
+    /**
+     * @dataProvider statuses
+     */
+    public function testReturnsExpectedExitCodeBasedOnEventDispatchStatus(
+        bool $failureStatus,
+        int $expectedStatus
+    ) {
+        $input      = $this->input;
+        $output     = $this->output;
+        $dispatcher = $this->dispatcher;
+
+        $event = $this->prophesize(EditChangelogLinksEvent::class);
+        $event->failed()->willReturn($failureStatus);
+
+        $dispatcher
+            ->dispatch(Argument::that(
+                function ($event) use ($input, $output, $dispatcher) {
+                    TestCase::assertInstanceOf(EditChangelogLinksEvent::class, $event);
+                    TestCase::assertSame($input->reveal(), $event->input());
+                    TestCase::assertSame($output->reveal(), $event->output());
+                    TestCase::assertSame($dispatcher->reveal(), $event->dispatcher());
+
+                    return $event;
+                }
+            ))
+            ->will(function () use ($event) {
+                return $event->reveal();
+            });
+
+        $command = new EditLinksCommand($dispatcher->reveal());
+
+        $this->assertSame($expectedStatus, $this->executeCommand($command));
+    }
+}
