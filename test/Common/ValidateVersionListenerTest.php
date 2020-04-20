d3570d200afb611204f2d7c63a65508c96da5aa6
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-04-20 10:42:27 +0200

    fix: rename test cases
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/Common/ValidateVersionListenerTest.php
+++ test/Common/ValidateVersionListenerTest.php
@@ -30,51 +30,51 @@ class ValidateVersionListenerTest extends TestCase
 
     public function versions() : iterable
     {
-        yield 'major-only'          => ['1', $isFailure = true];
-        yield 'minor-only'          => ['1.1', $isFailure = true];
-        yield 'malformed'           => ['1.1.1abcde', $isFailure = true];
+        yield 'major-only'              => ['1', $isFailure = true];
+        yield 'minor-only'              => ['1.1', $isFailure = true];
+        yield 'malformed'               => ['1.1.1abcde', $isFailure = true];
 
-        yield 'bugfix'              => ['1.1.1', $isFailure = false];
-        yield 'minor'               => ['1.2.0', $isFailure = false];
-        yield 'major'               => ['2.0.0', $isFailure = false];
+        yield 'bugfix'                  => ['1.1.1', $isFailure = false];
+        yield 'minor'                   => ['1.2.0', $isFailure = false];
+        yield 'major'                   => ['2.0.0', $isFailure = false];
 
-        yield 'alpha-wrong'         => ['2.0.0alphaBeta', $isFailure = true];
-        yield 'alpha-long'          => ['2.0.0alpha1', $isFailure = false];
-        yield 'alpha-short'         => ['2.0.0a1', $isFailure = false];
-        yield 'alpha-long-uc'       => ['2.0.0ALPHA1', $isFailure = false];
-        yield 'alpha-short-uc'      => ['2.0.0A1', $isFailure = false];
-        yield 'alpha-long-dash'     => ['2.0.0-alpha1', $isFailure = false];
-        yield 'alpha-long-dash-dot' => ['2.0.0-alpha.4', $isFailure = false];
+        yield 'alpha-wrong'             => ['2.0.0alphaBeta', $isFailure = true];
+        yield 'alpha-long'              => ['2.0.0alpha1', $isFailure = false];
+        yield 'alpha-short'             => ['2.0.0a1', $isFailure = false];
+        yield 'alpha-long-uc'           => ['2.0.0ALPHA1', $isFailure = false];
+        yield 'alpha-short-uc'          => ['2.0.0A1', $isFailure = false];
+        yield 'alpha-long-hyphen'       => ['2.0.0-alpha1', $isFailure = false];
+        yield 'alpha-long-hyphen-dot'   => ['2.0.0-alpha.4', $isFailure = false];
 
-        yield 'beta-wrong'          => ['2.0.0betaRC', $isFailure = true];
-        yield 'beta-long'           => ['2.0.0beta2', $isFailure = false];
-        yield 'beta-short'          => ['2.0.0b2', $isFailure = false];
-        yield 'beta-long-uc'        => ['2.0.0BETA2', $isFailure = false];
-        yield 'beta-short-uc'       => ['2.0.0B2', $isFailure = false];
-        yield 'beta-long-dash'      => ['2.0.0-beta2', $isFailure = false];
-        yield 'beta-long-dash-dot'  => ['2.0.0-beta.4', $isFailure = false];
+        yield 'beta-wrong'              => ['2.0.0betaRC', $isFailure = true];
+        yield 'beta-long'               => ['2.0.0beta2', $isFailure = false];
+        yield 'beta-short'              => ['2.0.0b2', $isFailure = false];
+        yield 'beta-long-uc'            => ['2.0.0BETA2', $isFailure = false];
+        yield 'beta-short-uc'           => ['2.0.0B2', $isFailure = false];
+        yield 'beta-long-hyphen'        => ['2.0.0-beta2', $isFailure = false];
+        yield 'beta-long-hyphen-dot'    => ['2.0.0-beta.4', $isFailure = false];
 
-        yield 'rc-wrong'            => ['2.0.0rcDEV', $isFailure = true];
-        yield 'rc-long'             => ['2.0.0rc3', $isFailure = false];
-        yield 'rc-long-uc'          => ['2.0.0RC3', $isFailure = false];
-        yield 'rc-long-dash'        => ['2.0.0-rc3', $isFailure = false];
-        yield 'rc-long-dash-dot'    => ['2.0.0-rc.3', $isFailure = false];
+        yield 'rc-wrong'                => ['2.0.0rcDEV', $isFailure = true];
+        yield 'rc-long'                 => ['2.0.0rc3', $isFailure = false];
+        yield 'rc-long-uc'              => ['2.0.0RC3', $isFailure = false];
+        yield 'rc-long-hyphen'          => ['2.0.0-rc3', $isFailure = false];
+        yield 'rc-long-hyphen-dot'      => ['2.0.0-rc.3', $isFailure = false];
 
-        yield 'dev-wrong'           => ['2.0.0devPATCH', $isFailure = true];
-        yield 'dev-long'            => ['2.0.0dev4', $isFailure = false];
-        yield 'dev-long-uc'         => ['2.0.0DEV4', $isFailure = false];
-        yield 'dev-long-dash'       => ['2.0.0-dev4', $isFailure = false];
-        yield 'dev-long-dash-dot'   => ['2.0.0-dev.4', $isFailure = false];
+        yield 'dev-wrong'               => ['2.0.0devPATCH', $isFailure = true];
+        yield 'dev-long'                => ['2.0.0dev4', $isFailure = false];
+        yield 'dev-long-uc'             => ['2.0.0DEV4', $isFailure = false];
+        yield 'dev-long-hyphen'         => ['2.0.0-dev4', $isFailure = false];
+        yield 'dev-long-hyphen-dot'     => ['2.0.0-dev.4', $isFailure = false];
 
-        yield 'patch-wrong'         => ['2.0.0patchFOO', $isFailure = true];
-        yield 'patch-long'          => ['2.0.0patch5', $isFailure = false];
-        yield 'patch-short'         => ['2.0.0pl5', $isFailure = false];
-        yield 'patch-shorter'       => ['2.0.0p5', $isFailure = false];
-        yield 'patch-long-uc'       => ['2.0.0PATCH5', $isFailure = false];
-        yield 'patch-short-uc'      => ['2.0.0PL5', $isFailure = false];
-        yield 'patch-shorter-uc'    => ['2.0.0P5', $isFailure = false];
-        yield 'patch-long-dash'     => ['2.0.0-patch4', $isFailure = false];
-        yield 'patch-long-dash-dot' => ['2.0.0-patch.4', $isFailure = false];
+        yield 'patch-wrong'             => ['2.0.0patchFOO', $isFailure = true];
+        yield 'patch-long'              => ['2.0.0patch5', $isFailure = false];
+        yield 'patch-short'             => ['2.0.0pl5', $isFailure = false];
+        yield 'patch-shorter'           => ['2.0.0p5', $isFailure = false];
+        yield 'patch-long-uc'           => ['2.0.0PATCH5', $isFailure = false];
+        yield 'patch-short-uc'          => ['2.0.0PL5', $isFailure = false];
+        yield 'patch-shorter-uc'        => ['2.0.0P5', $isFailure = false];
+        yield 'patch-long-hyphen'       => ['2.0.0-patch4', $isFailure = false];
+        yield 'patch-long-hyphen-dot'   => ['2.0.0-patch.4', $isFailure = false];
     }
 
     /**
