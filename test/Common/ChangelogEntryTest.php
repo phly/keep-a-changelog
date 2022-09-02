0af7425d11f3dc13ea2b300a587d60949d74fb34
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-09-02 08:47:39 -0500

    qa: apply automated phpcs fixes
    Removes `@license` and `@copyright` annotations

--- test/Common/ChangelogEntryTest.php
+++ test/Common/ChangelogEntryTest.php
@@ -2,8 +2,6 @@
 
 /**
  * @see       https://github.com/phly/keep-a-changelog for the canonical source repository
- * @copyright Copyright (c) 2019 Matthew Weier O'Phinney
- * @license   https://github.com/phly/keep-a-changelog/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
