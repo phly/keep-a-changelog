858554aa4bd75cee6bc0704f60c435b0be6362bb
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-09 11:48:51 -0600

    refactor: use native PHPUnit mocks with ReleaseCommandTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Version/ReleaseCommandTest.php
+++ test/Version/ReleaseCommandTest.php
@@ -10,9 +10,8 @@ namespace PhlyTest\KeepAChangelog\Version;
 
 use Phly\KeepAChangelog\Version\ReleaseCommand;
 use Phly\KeepAChangelog\Version\ReleaseEvent;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use ReflectionMethod;
 use Symfony\Component\Console\Input\InputInterface;
@@ -20,38 +19,41 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ReleaseCommandTest extends TestCase
 {
-    use ProphecyTrait;
+    private InputInterface&MockObject $input;
+    private OutputInterface&MockObject $output;
+    private EventDispatcherInterface&MockObject $dispatcher;
+    private ReleaseEvent&MockObject $event;
 
     protected function setUp(): void
     {
-        $this->input      = $this->prophesize(InputInterface::class);
-        $this->output     = $this->prophesize(OutputInterface::class);
-        $this->dispatcher = $this->prophesize(EventDispatcherInterface::class);
-        $this->event      = $this->prophesize(ReleaseEvent::class);
+        $this->input      = $this->createMock(InputInterface::class);
+        $this->output     = $this->createMock(OutputInterface::class);
+        $this->dispatcher = $this->createMock(EventDispatcherInterface::class);
+        $this->event      = $this->createMock(ReleaseEvent::class);
     }
 
     public function executeCommand(ReleaseCommand $command): int
     {
         $r = new ReflectionMethod($command, 'execute');
         $r->setAccessible(true);
-        return $r->invoke($command, $this->input->reveal(), $this->output->reveal());
+        return $r->invoke($command, $this->input, $this->output);
     }
 
     public function createCommand(): ReleaseCommand
     {
-        return new ReleaseCommand($this->dispatcher->reveal());
+        return new ReleaseCommand($this->dispatcher);
     }
 
     public function testExecutionReturnsFailedStatusWhenEventReturnsFailedStatus()
     {
         $event = $this->event;
-        $event->failed()->willReturn(true);
+        $event->expects($this->atLeastOnce())->method('failed')->willReturn(true);
 
         $this->dispatcher
-            ->dispatch(Argument::type(ReleaseEvent::class))
-            ->will(function () use ($event) {
-                return $event->reveal();
-            });
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(ReleaseEvent::class))
+            ->willReturn($event);
 
         $command = $this->createCommand();
 
@@ -61,13 +63,13 @@ class ReleaseCommandTest extends TestCase
     public function testExecutionReturnsSuccessStatusWhenEventDoesNotFail()
     {
         $event = $this->event;
-        $event->failed()->willReturn(false);
+        $event->expects($this->atLeastOnce())->method('failed')->willReturn(false);
 
         $this->dispatcher
-            ->dispatch(Argument::type(ReleaseEvent::class))
-            ->will(function () use ($event) {
-                return $event->reveal();
-            });
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(ReleaseEvent::class))
+            ->willReturn($event);
 
         $command = $this->createCommand();
 
