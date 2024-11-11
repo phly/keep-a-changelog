0f8a95ad656293e99adaad5ec21e740bbdb2ddac
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-11 10:38:24 -0600

    refactor: use native PHPUnit mocks with ConfigListenerTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Config/ConfigListenerTest.php
+++ test/Config/ConfigListenerTest.php
@@ -14,9 +14,8 @@ use Phly\KeepAChangelog\Config\ConfigDiscovery;
 use Phly\KeepAChangelog\Config\ConfigListener;
 use Phly\KeepAChangelog\Config\PackageNameDiscovery;
 use Phly\KeepAChangelog\Config\RemoteNameDiscovery;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,20 +23,26 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ConfigListenerTest extends TestCase
 {
-    use ProphecyTrait;
+    private Config $config;
+    private ConfigDiscovery&MockObject $discovery;
+    private EventDispatcherInterface&MockObject $dispatcher;
+    private InputInterface&MockObject $input;
+    private OutputInterface&MockObject $output;
+    private EventInterface&MockObject $event;
 
     protected function setUp(): void
     {
-        $this->config    = new Config();
-        $this->discovery = $this->prophesize(ConfigDiscovery::class);
-        $this->discovery->config()->willReturn($this->config);
-        $this->dispatcher = $this->prophesize(EventDispatcherInterface::class);
-        $this->input      = $this->prophesize(InputInterface::class)->reveal();
-        $this->output     = $this->prophesize(OutputInterface::class);
-        $this->event      = $this->prophesize(EventInterface::class);
-        $this->event->input()->willReturn($this->input);
-        $this->event->output()->will([$this->output, 'reveal']);
-        $this->event->dispatcher()->will([$this->dispatcher, 'reveal']);
+        $this->config     = new Config();
+        $this->discovery  = $this->createMock(ConfigDiscovery::class);
+        $this->dispatcher = $this->createMock(EventDispatcherInterface::class);
+        $this->input      = $this->createMock(InputInterface::class);
+        $this->output     = $this->createMock(OutputInterface::class);
+        $this->event      = $this->createMock(EventInterface::class);
+
+        $this->discovery->expects($this->any())->method('config')->willReturn($this->config);
+        $this->event->expects($this->any())->method('input')->willReturn($this->input);
+        $this->event->expects($this->any())->method('output')->willReturn($this->output);
+        $this->event->expects($this->any())->method('dispatcher')->willReturn($this->dispatcher);
     }
 
     public function createListener(
@@ -53,115 +58,155 @@ class ConfigListenerTest extends TestCase
     public function testTriggersConfigDiscoveryAndMarksConfigurationDiscoveredWhenNeitherPackageNorRemoteRequired()
     {
         $this->dispatcher
-            ->dispatch(Argument::type(ConfigDiscovery::class))
-            ->will([$this->discovery, 'reveal']);
-
-        $this->event
-            ->discoveredConfiguration($this->config)
-            ->shouldBeCalled();
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(ConfigDiscovery::class))
+            ->willReturn($this->discovery);
+        $this->output->expects($this->never())->method('writeln');
+        $this->event->expects($this->once())->method('discoveredConfiguration')->with($this->config);
 
         $listener = $this->createListener();
 
-        $this->assertNull($listener($this->event->reveal()));
-        $this->dispatcher->dispatch(Argument::type(PackageNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->dispatcher->dispatch(Argument::type(RemoteNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->output->writeln(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testNoPackageCheckPerformedIfPackageAlreadyExistsInConfig()
     {
         $this->config->setPackage('some/package');
-
         $this->dispatcher
-            ->dispatch(Argument::type(ConfigDiscovery::class))
-            ->will([$this->discovery, 'reveal']);
-
-        $this->event
-            ->discoveredConfiguration($this->config)
-            ->shouldBeCalled();
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(ConfigDiscovery::class))
+            ->willReturn($this->discovery);
+        $this->output->expects($this->never())->method('writeln');
+        $this->event->expects($this->once())->method('discoveredConfiguration')->with($this->config);
 
         $listener = $this->createListener($requiresPackageName = true);
 
-        $this->assertNull($listener($this->event->reveal()));
-        $this->dispatcher->dispatch(Argument::type(PackageNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->dispatcher->dispatch(Argument::type(RemoteNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->output->writeln(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testNoRemoteCheckPerformedIfPackageAlreadyExistsInConfig()
     {
         $this->config->setRemote('upstream');
-
         $this->dispatcher
-            ->dispatch(Argument::type(ConfigDiscovery::class))
-            ->will([$this->discovery, 'reveal']);
-
-        $this->event
-            ->discoveredConfiguration($this->config)
-            ->shouldBeCalled();
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(ConfigDiscovery::class))
+            ->willReturn($this->discovery);
+        $this->output->expects($this->never())->method('writeln');
+        $this->event->expects($this->once())->method('discoveredConfiguration')->with($this->config);
 
         $listener = $this->createListener($requiresPackageName = false, $requiresRemoteName = true);
 
-        $this->assertNull($listener($this->event->reveal()));
-        $this->dispatcher->dispatch(Argument::type(PackageNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->dispatcher->dispatch(Argument::type(RemoteNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->output->writeln(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testMarksConfigurationIncompleteIfPackageNameDiscoveryFails()
     {
+        $invokedCounter = $this->exactly(2);
         $this->dispatcher
-            ->dispatch(Argument::type(ConfigDiscovery::class))
-            ->will([$this->discovery, 'reveal']);
+            ->expects($invokedCounter)
+            ->method('dispatch')
+            ->will($this->returnCallback(function ($event) use ($invokedCounter) {
+                $invocationCount = $invokedCounter->getInvocationCount();
+                if (1 === $invocationCount) {
+                    TestCase::assertInstanceOf(ConfigDiscovery::class, $event);
+                    return $this->discovery;
+                }
+                if (2 === $invocationCount) {
+                    TestCase::assertInstanceOf(PackageNameDiscovery::class, $event);
+                    return new PackageNameDiscovery($this->input, $this->output, $this->config);
+                }
+                return null;
+            }));
+        $this->event->expects($this->never())->method('discoveredConfiguration');
+        $this->event->expects($this->once())->method('configurationIncomplete');
 
-        $this->dispatcher
-            ->dispatch(Argument::type(PackageNameDiscovery::class))
-            ->willReturn(new PackageNameDiscovery(
-                $this->input,
-                $this->output->reveal(),
-                $this->config
-            ));
-
-        $this->output->writeln(Argument::containingString('Unable to determine package name'))->shouldBeCalled();
-        $this->output->writeln(Argument::containingString('do one of the following'))->shouldBeCalled();
-        $this->output->writeln(Argument::containingString('Add a "package" setting'))->shouldBeCalled();
-        $this->output->writeln(Argument::containingString('--package option'))->shouldBeCalled();
-
-        $this->event->configurationIncomplete()->shouldBeCalled();
+        $expectedStrings = [
+            'Unable to determine package name' => false,
+            'do one of the following'          => false,
+            'Add a "package" setting'          => false,
+            '--package option'                 => false,
+        ];
+        $this->output
+            ->expects($this->atLeast(4))
+            ->method('writeln')
+            ->with($this->callback(function (string $string) use (&$expectedStrings): bool {
+                foreach (array_keys($expectedStrings) as $expectedString) {
+                    if (str_contains($string, $expectedString)) {
+                        $expectedStrings[$expectedString] = true;
+                        return true;
+                    }
+                }
+                return true;
+            }));
 
         $listener = $this->createListener($requiresPackageName = true);
 
-        $this->assertNull($listener($this->event->reveal()));
-        $this->dispatcher->dispatch(Argument::type(RemoteNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->event->discoveredConfiguration(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
+        $this->assertAllExpectedOutputEmitted($expectedStrings);
     }
 
     public function testMarksConfigurationIncompleteIfRemoteNameDiscoveryFails()
     {
+        $invokedCounter = $this->exactly(2);
         $this->dispatcher
-            ->dispatch(Argument::type(ConfigDiscovery::class))
-            ->will([$this->discovery, 'reveal']);
+            ->expects($invokedCounter)
+            ->method('dispatch')
+            ->will($this->returnCallback(function ($event) use ($invokedCounter) {
+                $invocationCount = $invokedCounter->getInvocationCount();
+                if (1 === $invocationCount) {
+                    TestCase::assertInstanceOf(ConfigDiscovery::class, $event);
+                    return $this->discovery;
+                }
+                if (2 === $invocationCount) {
+                    TestCase::assertInstanceOf(RemoteNameDiscovery::class, $event);
+                    return new RemoteNameDiscovery($this->input, $this->output, $this->config, new QuestionHelper());
+                }
+                return null;
+            }));
+        $expectedStrings = [
+            'Unable to determine Git remote' => false,
+            'do one of the following'        => false,
+            'Add a "remote" setting'         => false,
+            '--remote option'                => false,
+        ];
+        $this->output
+            ->expects($this->atLeast(4))
+            ->method('writeln')
+            ->with($this->callback(function (string $string) use (&$expectedStrings): bool {
+                foreach (array_keys($expectedStrings) as $expectedString) {
+                    if (str_contains($string, $expectedString)) {
+                        $expectedStrings[$expectedString] = true;
+                        return true;
+                    }
+                }
+                return true;
+            }));
 
-        $this->dispatcher
-            ->dispatch(Argument::type(RemoteNameDiscovery::class))
-            ->willReturn(new RemoteNameDiscovery(
-                $this->input,
-                $this->output->reveal(),
-                $this->config,
-                new QuestionHelper()
-            ));
-
-        $this->output->writeln(Argument::containingString('Unable to determine Git remote'))->shouldBeCalled();
-        $this->output->writeln(Argument::containingString('do one of the following'))->shouldBeCalled();
-        $this->output->writeln(Argument::containingString('Add a "remote" setting'))->shouldBeCalled();
-        $this->output->writeln(Argument::containingString('--remote option'))->shouldBeCalled();
-
-        $this->event->configurationIncomplete()->shouldBeCalled();
+        $this->event->expects($this->never())->method('discoveredConfiguration');
+        $this->event->expects($this->once())->method('configurationIncomplete');
 
         $listener = $this->createListener($requiresPackageName = false, $requiresRemoteName = true);
 
-        $this->assertNull($listener($this->event->reveal()));
-        $this->dispatcher->dispatch(Argument::type(PackageNameDiscovery::class))->shouldNotHaveBeenCalled();
-        $this->event->discoveredConfiguration(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
+        $this->assertAllExpectedOutputEmitted($expectedStrings);
+    }
+
+    private function assertAllExpectedOutputEmitted(array $expectedStrings): void
+    {
+        $notFound = [];
+        foreach ($expectedStrings as $string => $found) {
+            if (! $found) {
+                $notFound[] = $string;
+            }
+        }
+
+        if (count($notFound) === 0) {
+            return;
+        }
+
+        $this->fail(sprintf('One or more expected output strings were not emitted: %s', implode(', ', $notFound)));
     }
 }
