63e68ce9d817653b2a8290ba4ada54a5359f825f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-11 15:31:10 -0600

    refactor: use native PHPUnit mocks with DiscoverChangelogEntryListenerTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Common/DiscoverChangelogEntryListenerTest.php
+++ test/Common/DiscoverChangelogEntryListenerTest.php
@@ -8,136 +8,135 @@ declare(strict_types=1);
 
 namespace PhlyTest\KeepAChangelog\Common;
 
+use Phly\KeepAChangelog\Common\ChangelogEntry;
 use Phly\KeepAChangelog\Common\ChangelogEntryAwareEventInterface;
 use Phly\KeepAChangelog\Common\DiscoverChangelogEntryListener;
 use Phly\KeepAChangelog\Config;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
 
 class DiscoverChangelogEntryListenerTest extends TestCase
 {
-    use ProphecyTrait;
+    private string $changelog;
+    private Config&MockObject $config;
+    private ChangelogEntryAwareEventInterface&MockObject $event;
 
     protected function setUp(): void
     {
-        $voidReturn = function () {
-        };
-
         $this->changelog = __DIR__ . '/../_files/CHANGELOG.md';
 
-        $this->config = $this->prophesize(Config::class);
-        $this->config->changelogFile()->willReturn($this->changelog);
+        $this->config = $this->createMock(Config::class);
+        $this->config->expects($this->any())->method('changelogFile')->willReturn($this->changelog);
 
-        $this->event = $this->prophesize(ChangelogEntryAwareEventInterface::class);
-        $this->event->config()->will([$this->config, 'reveal']);
+        $this->event = $this->createMock(ChangelogEntryAwareEventInterface::class);
+        $this->event->expects($this->any())->method('config')->willReturn($this->config);
     }
 
     public function testNotifiesEventWhenEntryNotFound()
     {
-        $this->event->version()->willReturn('7.6.5');
-        $this->event
-            ->changelogEntryNotFound($this->changelog, '7.6.5')
-            ->shouldBeCalled();
+        $this->event->expects($this->once())->method('version')->willReturn('7.6.5');
+        $this->event->expects($this->once())->method('changelogEntryNotFound')->with($this->changelog, '7.6.5');
+        $this->event->expects($this->never())->method('discoveredChangelogEntry');
 
         $listener = new DiscoverChangelogEntryListener();
 
-        $this->assertNull($listener($this->event->reveal()));
-        $this->event->discoveredChangelogEntry(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testNotifiesEventWithDiscoveredEntryOnSuccess()
     {
-        $this->event->version()->willReturn('1.1.0');
         $expected = <<<'EOC'
-## 1.1.0 - 2018-03-23
+            ## 1.1.0 - 2018-03-23
+            
+            ### Added
+            
+            - Added a new feature.
+            
+            ### Changed
+            
+            - Made some changes.
+            
+            ### Deprecated
+            
+            - Nothing was deprecated.
+            
+            ### Removed
+            
+            - Nothing was removed.
+            
+            ### Fixed
+            
+            - Fixed some bugs.
+            
+            
+            EOC;
 
-### Added
-
-- Added a new feature.
-
-### Changed
-
-- Made some changes.
-
-### Deprecated
-
-- Nothing was deprecated.
-
-### Removed
-
-- Nothing was removed.
-
-### Fixed
-
-- Fixed some bugs.
-
-
-EOC;
+        $this->event->expects($this->once())->method('version')->willReturn('1.1.0');
         $this->event
-            ->discoveredChangelogEntry(Argument::that(function ($entry) use ($expected) {
+            ->expects($this->once())
+            ->method('discoveredChangelogEntry')
+            ->with($this->callback(function (ChangelogEntry $entry) use ($expected): bool {
                 TestCase::assertSame(26, $entry->index);
                 TestCase::assertSame(22, $entry->length);
                 TestCase::assertSame($expected, $entry->contents);
-                return $entry;
-            }))
-            ->shouldBeCalled();
+                return true;
+            }));
+        $this->event->expects($this->never())->method('changelogEntryNotFound');
 
         $listener = new DiscoverChangelogEntryListener();
 
-        $this->assertNull($listener($this->event->reveal()));
-        $this->event->changelogEntryNotFound(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testOmitsLinksWhenReturningLastEntryInFile()
     {
         $expected  = <<<'EOC'
-## [0.1.0] - 2018-03-23
-
-### Added
-
-- Nothing.
-
-### Changed
-
-- Nothing.
-
-### Deprecated
-
-- Nothing.
-
-### Removed
-
-- Nothing.
-
-### Fixed
-
-- Nothing.
-
-
-EOC;
+            ## [0.1.0] - 2018-03-23
+        
+            ### Added
+        
+            - Nothing.
+        
+            ### Changed
+        
+            - Nothing.
+        
+            ### Deprecated
+        
+            - Nothing.
+        
+            ### Removed
+        
+            - Nothing.
+        
+            ### Fixed
+        
+            - Nothing.
+        
+        
+            EOC;
         $changelog = __DIR__ . '/../_files/CHANGELOG-WITH-LINKS.md';
 
-        $config = $this->prophesize(Config::class);
-        $config->changelogFile()->willReturn($changelog);
+        $config = $this->createMock(Config::class);
+        $config->expects($this->once())->method('changelogFile')->willReturn($changelog);
 
-        $event = $this->prophesize(ChangelogEntryAwareEventInterface::class);
-        $event->config()->will([$config, 'reveal']);
-        $event->version()->willReturn('0.1.0');
-
+        $event = $this->createMock(ChangelogEntryAwareEventInterface::class);
+        $event->expects($this->atLeastOnce())->method('config')->willReturn($config);
+        $event->expects($this->atLeastOnce())->method('version')->willReturn('0.1.0');
+        $event->expects($this->never())->method('changelogEntryNotFound');
         $event
-            ->discoveredChangelogEntry(Argument::that(function ($entry) use ($expected) {
+            ->expects($this->once())
+            ->method('discoveredChangelogEntry')
+            ->with($this->callback(function (ChangelogEntry $entry) use ($expected): bool {
                 TestCase::assertSame(48, $entry->index);
                 TestCase::assertSame(22, $entry->length);
                 TestCase::assertSame($expected, $entry->contents);
-                return $entry;
-            }))
-            ->shouldBeCalled();
+                return true;
+            }));
 
         $listener = new DiscoverChangelogEntryListener();
 
-        $this->assertNull($listener($event->reveal()));
-        $event->changelogEntryNotFound(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($event));
     }
 
     public function unreleasedVersions(): iterable
@@ -152,52 +151,52 @@ EOC;
     public function testNotifiesEventWithDiscoveredEntryWhenUnreleasedSectionFound(?string $version): void
     {
         $changelog = __DIR__ . '/../_files/CHANGELOG-WITH-UNRELEASED-SECTION.md';
+        $expected  = <<<'EOC'
+            ## Unreleased
+            
+            ### Added
+            
+            - Nothing.
+            
+            ### Changed
+            
+            - Nothing.
+            
+            ### Deprecated
+            
+            - Nothing.
+            
+            ### Removed
+            
+            - Nothing.
+            
+            ### Fixed
+            
+            - Nothing.
+            
+            
+            EOC;
 
-        $config = $this->prophesize(Config::class);
-        $config->changelogFile()->willReturn($changelog);
+        $config = $this->createMock(Config::class);
+        $config->expects($this->once())->method('changelogFile')->willReturn($changelog);
 
-        $event = $this->prophesize(ChangelogEntryAwareEventInterface::class);
-        $event->config()->will([$config, 'reveal']);
-        $event->version()->willReturn($version);
-
-        $expected = <<<'EOC'
-## Unreleased
-
-### Added
-
-- Nothing.
-
-### Changed
-
-- Nothing.
-
-### Deprecated
-
-- Nothing.
-
-### Removed
-
-- Nothing.
-
-### Fixed
-
-- Nothing.
-
-
-EOC;
+        $event = $this->createMock(ChangelogEntryAwareEventInterface::class);
+        $event->expects($this->atLeastOnce())->method('config')->willReturn($config);
+        $event->expects($this->atLeastOnce())->method('version')->willReturn($version);
+        $event->expects($this->never())->method('changelogEntryNotFound');
         $event
-            ->discoveredChangelogEntry(Argument::that(function ($entry) use ($expected) {
+            ->expects($this->once())
+            ->method('discoveredChangelogEntry')
+            ->with($this->callback(function (ChangelogEntry $entry) use ($expected): bool {
                 TestCase::assertSame(4, $entry->index, $entry->contents);
                 TestCase::assertSame(22, $entry->length);
                 TestCase::assertSame($expected, $entry->contents);
-                return $entry;
-            }))
-            ->shouldBeCalled();
+                return true;
+            }));
 
         $listener = new DiscoverChangelogEntryListener();
 
-        $this->assertNull($listener($event->reveal()));
-        $this->event->changelogEntryNotFound(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($event));
     }
 
     /**
@@ -206,51 +205,51 @@ EOC;
     public function testNotifiesEventWithDiscoveredEntryWhenLinkedUnreleasedSectionFound(?string $version): void
     {
         $changelog = __DIR__ . '/../_files/CHANGELOG-WITH-LINKED-UNRELEASED-SECTION.md';
+        $expected  = <<<'EOC'
+            ## [Unreleased]
+            
+            ### Added
+            
+            - Nothing.
+            
+            ### Changed
+            
+            - Nothing.
+            
+            ### Deprecated
+            
+            - Nothing.
+            
+            ### Removed
+            
+            - Nothing.
+            
+            ### Fixed
+            
+            - Nothing.
+            
+            
+            EOC;
 
-        $config = $this->prophesize(Config::class);
-        $config->changelogFile()->willReturn($changelog);
+        $config = $this->createMock(Config::class);
+        $config->expects($this->once())->method('changelogFile')->willReturn($changelog);
 
-        $event = $this->prophesize(ChangelogEntryAwareEventInterface::class);
-        $event->config()->will([$config, 'reveal']);
-        $event->version()->willReturn($version);
-
-        $expected = <<<'EOC'
-## [Unreleased]
-
-### Added
-
-- Nothing.
-
-### Changed
-
-- Nothing.
-
-### Deprecated
-
-- Nothing.
-
-### Removed
-
-- Nothing.
-
-### Fixed
-
-- Nothing.
-
-
-EOC;
+        $event = $this->createMock(ChangelogEntryAwareEventInterface::class);
+        $event->expects($this->atLeastOnce())->method('config')->willReturn($config);
+        $event->expects($this->atLeastOnce())->method('version')->willReturn($version);
+        $event->expects($this->never())->method('changelogEntryNotFound');
         $event
-            ->discoveredChangelogEntry(Argument::that(function ($entry) use ($expected) {
+            ->expects($this->once())
+            ->method('discoveredChangelogEntry')
+            ->with($this->callback(function (ChangelogEntry $entry) use ($expected): bool {
                 TestCase::assertSame(4, $entry->index, $entry->contents);
                 TestCase::assertSame(22, $entry->length);
                 TestCase::assertSame($expected, $entry->contents);
-                return $entry;
-            }))
-            ->shouldBeCalled();
+                return true;
+            }));
 
         $listener = new DiscoverChangelogEntryListener();
 
-        $this->assertNull($listener($event->reveal()));
-        $this->event->changelogEntryNotFound(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($event));
     }
 }
