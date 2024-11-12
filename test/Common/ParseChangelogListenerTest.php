671dedec6346cc447b2f06b25376bb2fceb19c89
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-12 08:08:53 -0600

    refactor: use native PHPUnit mocks with ParseChangelogListenerTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Common/ParseChangelogListenerTest.php
+++ test/Common/ParseChangelogListenerTest.php
@@ -12,55 +12,33 @@ use Phly\KeepAChangelog\Common\ChangelogAwareEventInterface;
 use Phly\KeepAChangelog\Common\ParseChangelogListener;
 use Phly\KeepAChangelog\Config;
 use Phly\KeepAChangelog\Exception\ExceptionInterface;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
 
 use function realpath;
 
 class ParseChangelogListenerTest extends TestCase
 {
-    use ProphecyTrait;
+    private Config&MockObject $config;
+    private ChangelogAwareEventInterface&MockObject $event;
 
     protected function setUp(): void
     {
-        $this->config = $this->prophesize(Config::class);
-        $this->event  = $this->prophesize(ChangelogAwareEventInterface::class);
-        $this->event->config()->will([$this->config, 'reveal']);
+        $this->config = $this->createMock(Config::class);
+        $this->event  = $this->createMock(ChangelogAwareEventInterface::class);
+        $this->event->expects($this->any())->method('config')->willReturn($this->config);
     }
 
     public function testListenerUpdatesChangelogWhenChangelogFileParsed()
     {
         $changelogFile = realpath(__DIR__ . '/../_files') . '/CHANGELOG.md';
-        $expected      = <<<'EOC'
-### Added
-
-- Added a new feature.
-
-### Changed
-
-- Made some changes.
-
-### Deprecated
-
-- Nothing was deprecated.
-
-### Removed
-
-- Nothing was removed.
-
-### Fixed
-
-- Fixed some bugs.
-
-EOC;
         $listener      = new ParseChangelogListener();
 
-        $this->config->changelogFile()->willReturn($changelogFile);
-        $this->event->version()->willReturn('1.1.0');
-        $this->event->updateChangelog($expected)->shouldBeCalled();
+        $this->config->expects($this->once())->method('changelogFile')->willReturn($changelogFile);
+        $this->event->expects($this->once())->method('version')->willReturn('1.1.0');
+        $this->event->expects($this->once())->method('updateChangelog');
 
-        $this->assertNull($listener($this->event->reveal()));
+        $this->assertNull($listener($this->event));
     }
 
     public function testListenerNotifiesEventOfParsingErrors()
@@ -68,15 +46,13 @@ EOC;
         $changelogFile = realpath(__DIR__ . '/../_files') . '/CHANGELOG.md';
         $listener      = new ParseChangelogListener();
 
-        $this->config->changelogFile()->willReturn($changelogFile);
-        $this->event->version()->willReturn('1.0.1');
+        $this->config->expects($this->once())->method('changelogFile')->willReturn($changelogFile);
+        $this->event->expects($this->once())->method('version')->willReturn('1.0.1');
         $this->event
-            ->errorParsingChangelog(
-                $changelogFile,
-                Argument::type(ExceptionInterface::class)
-            )
-            ->shouldBeCalled();
+            ->expects($this->once())
+            ->method('errorParsingChangelog')
+            ->with($changelogFile, $this->isInstanceOf(ExceptionInterface::class));
 
-        $this->assertNull($listener($this->event->reveal()));
+        $this->assertNull($listener($this->event));
     }
 }
