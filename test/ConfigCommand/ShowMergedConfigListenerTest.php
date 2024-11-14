4d024672ee44713a64822a27b4ad226fb24cb329
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-14 11:06:48 -0600

    refactor: use native PHPUnit mocks with ShowMergedConfigListenerTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/ConfigCommand/ShowMergedConfigListenerTest.php
+++ test/ConfigCommand/ShowMergedConfigListenerTest.php
@@ -10,89 +10,76 @@ namespace PhlyTest\KeepAChangelog\ConfigCommand;
 
 use Phly\KeepAChangelog\ConfigCommand\ShowConfigEvent;
 use Phly\KeepAChangelog\ConfigCommand\ShowMergedConfigListener;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
 
 use function parse_ini_string;
 
 class ShowMergedConfigListenerTest extends TestCase
 {
-    use ProphecyTrait;
+    private ShowConfigEvent&MockObject $event;
 
     protected function setUp(): void
     {
-        $voidReturn = function () {
-        };
-
-        $this->event = $this->prophesize(ShowConfigEvent::class);
-        $this->event->configIsNotReadable(Argument::any(), 'global')->will($voidReturn);
-        $this->event->configIsNotReadable(Argument::any(), 'local')->will($voidReturn);
-        $this->event->displayMergedConfig(Argument::any())->will($voidReturn);
+        $this->event = $this->createMock(ShowConfigEvent::class);
     }
 
     public function testListenerReturnsEarlyIfMergedConfigIsNotRequested()
     {
-        $this->event->showMerged()->willReturn(false);
+        $this->event->expects($this->atLeastOnce())->method('showMerged')->willReturn(false);
+        $this->event->expects($this->never())->method('configIsNotReadable');
+        $this->event->expects($this->never())->method('displayMergedConfig');
 
         $listener = new ShowMergedConfigListener();
 
-        $this->assertNull($listener($this->event->reveal()));
-
-        $this->event->configIsNotReadable(Argument::any(), Argument::any())->shouldNotHaveBeenCalled();
-        $this->event->displayMergedConfig(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testListenerReturnsEarlyIfGlobalConfigIsNotReadable()
     {
-        $this->event->showMerged()->willReturn(true);
+        $this->event->expects($this->atLeastOnce())->method('showMerged')->willReturn(true);
+        $this->event->expects($this->once())->method('configIsNotReadable')->with($this->isType('string'), 'global');
+        $this->event->expects($this->never())->method('displayMergedConfig');
 
         $listener             = new ShowMergedConfigListener();
         $listener->configRoot = __DIR__;
 
-        $this->assertNull($listener($this->event->reveal()));
-
-        $this->event->configIsNotReadable(Argument::any(), 'global')->shouldHaveBeenCalled();
-        $this->event->configIsNotReadable(Argument::any(), 'local')->shouldNotHaveBeenCalled();
-        $this->event->displayMergedConfig(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testListenerReturnsEarlyIfLocalConfigIsNotReadable()
     {
-        $this->event->showMerged()->willReturn(true);
+        $this->event->expects($this->atLeastOnce())->method('showMerged')->willReturn(true);
+        $this->event->expects($this->once())->method('configIsNotReadable')->with($this->isType('string'), 'local');
+        $this->event->expects($this->never())->method('displayMergedConfig');
 
         $listener                  = new ShowMergedConfigListener();
         $listener->configRoot      = __DIR__ . '/../_files/config';
         $listener->localConfigRoot = __DIR__;
 
-        $this->assertNull($listener($this->event->reveal()));
-
-        $this->event->configIsNotReadable(Argument::any(), 'global')->shouldNotHaveBeenCalled();
-        $this->event->configIsNotReadable(Argument::any(), 'local')->shouldHaveBeenCalled();
-        $this->event->displayMergedConfig(Argument::any())->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testNotifiesEventToDisplayMergedConfig()
     {
-        $this->event->showMerged()->willReturn(true);
-
-        $listener                  = new ShowMergedConfigListener();
-        $listener->configRoot      = __DIR__ . '/../_files/config';
-        $listener->localConfigRoot = __DIR__ . '/../_files/config/local';
-
-        $this->assertNull($listener($this->event->reveal()));
-
-        $this->event->configIsNotReadable(Argument::any(), 'global')->shouldNotHaveBeenCalled();
-        $this->event->configIsNotReadable(Argument::any(), 'local')->shouldNotHaveBeenCalled();
+        $this->event->expects($this->atLeastOnce())->method('showMerged')->willReturn(true);
+        $this->event->expects($this->never())->method('configIsNotReadable');
         $this->event
-            ->displayMergedConfig(Argument::that(function ($configString) {
+            ->expects($this->once())
+            ->method('displayMergedConfig')
+            ->with($this->callback(function (string $configString): bool {
                 $config = parse_ini_string($configString, true);
                 TestCase::assertSame('CHANGELOG.txt', $config['defaults']['changelog_file']);
                 TestCase::assertSame('github', $config['defaults']['provider']);
                 TestCase::assertSame('origin', $config['defaults']['remote']);
                 TestCase::assertSame('https://github.mwop.net', $config['providers']['github']['url']);
-                return $configString;
-            }))
-            ->shouldHaveBeenCalled();
+                return true;
+            }));
+
+        $listener                  = new ShowMergedConfigListener();
+        $listener->configRoot      = __DIR__ . '/../_files/config';
+        $listener->localConfigRoot = __DIR__ . '/../_files/config/local';
+
+        $this->assertNull($listener($this->event));
     }
 }
