84f5722ccfbf26999115044ef923177ee0ecc208
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-15 11:53:30 -0600

    refactor: use native PHPUnit mocks with VerifyProviderListenerTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Milestone/VerifyProviderListenerTest.php
+++ test/Milestone/VerifyProviderListenerTest.php
@@ -11,75 +11,60 @@ namespace PhlyTest\KeepAChangelog\Milestone;
 use Phly\KeepAChangelog\Config;
 use Phly\KeepAChangelog\Milestone\AbstractMilestoneProviderEvent;
 use Phly\KeepAChangelog\Milestone\VerifyProviderListener;
-use Phly\KeepAChangelog\Provider\MilestoneAwareProviderInterface;
 use Phly\KeepAChangelog\Provider\ProviderInterface;
 use Phly\KeepAChangelog\Provider\ProviderSpec;
+use PhlyTest\KeepAChangelog\TestAsset\AbstractMilestoneAwareProvider;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 
 class VerifyProviderListenerTest extends TestCase
 {
-    use ProphecyTrait;
-
-    /** @var Config|ObjectProphecy */
-    private $config;
-
-    /** @var AbstractMilestoneProviderEvent|ObjectProphecy */
-    private $event;
-
-    /** @var VerifyProviderListener */
-    private $listener;
-
-    /** @var ProviderSpec|ObjectProphecy */
-    private $providerSpec;
+    private Config|MockObject $config;
+    private AbstractMilestoneProviderEvent&MockObject $event;
+    private VerifyProviderListener $listener;
+    private ProviderSpec&MockObject $providerSpec;
 
     public function setUp(): void
     {
-        $this->providerSpec = $this->prophesize(ProviderSpec::class);
+        $this->providerSpec = $this->createMock(ProviderSpec::class);
+        $this->config       = $this->createMock(Config::class);
+        $this->event        = $this->createMock(AbstractMilestoneProviderEvent::class);
+        $this->listener     = new VerifyProviderListener();
 
-        $this->config = $this->prophesize(Config::class);
-        $this->config->provider()->will([$this->providerSpec, 'reveal']);
-
-        $this->event = $this->prophesize(AbstractMilestoneProviderEvent::class);
-        $this->event->config()->will([$this->config, 'reveal']);
-
-        $this->listener = new VerifyProviderListener();
+        $this->config->expects($this->any())->method('provider')->willReturn($this->providerSpec);
+        $this->event->expects($this->any())->method('config')->willReturn($this->config);
     }
 
     public function testListenerMarksEventAsIncompleteIfProviderSpecIsIncomplete(): void
     {
-        $this->providerSpec->isComplete()->willReturn(false);
-        $this->providerSpec->createProvider()->shouldNotBeCalled();
-        $this->event->providerIsIncomplete()->shouldBeCalled();
-        $this->event->discoveredProvider(Argument::any())->shouldNotBeCalled();
+        $this->providerSpec->expects($this->once())->method('isComplete')->willReturn(false);
+        $this->providerSpec->expects($this->never())->method('createProvider');
+        $this->event->expects($this->once())->method('providerIsIncomplete');
+        $this->event->expects($this->never())->method('discoveredProvider');
 
-        $this->assertNull($this->listener->__invoke($this->event->reveal()));
+        $this->assertNull($this->listener->__invoke($this->event));
     }
 
     public function testListenerMarksEventInvalidIfProviderIsNotMilestoneAware(): void
     {
-        $provider = $this->prophesize(ProviderInterface::class)->reveal();
-        $this->providerSpec->isComplete()->willReturn(true)->shouldBeCalled();
-        $this->providerSpec->createProvider()->willReturn($provider)->shouldBeCalled();
+        $provider = $this->createMock(ProviderInterface::class);
 
-        $this->event->providerIncapableOfMilestones()->shouldBeCalled();
+        $this->providerSpec->expects($this->once())->method('isComplete')->willReturn(true);
+        $this->providerSpec->expects($this->once())->method('createProvider')->willReturn($provider);
+        $this->event->expects($this->once())->method('providerIncapableOfMilestones');
 
-        $this->assertNull($this->listener->__invoke($this->event->reveal()));
+        $this->assertNull($this->listener->__invoke($this->event));
     }
 
     public function testListenerTellsEventProviderIsDiscoveredWhenProviderSpecProvideMilestoneAwareProvider(): void
     {
-        $provider = $this->prophesize(MilestoneAwareProviderInterface::class)
-            ->willImplement(ProviderInterface::class)
-            ->reveal();
-        $this->providerSpec->isComplete()->willReturn(true);
-        $this->event->providerIsIncomplete()->shouldNotBeCalled();
-        $this->providerSpec->createProvider()->willReturn($provider)->shouldBeCalled();
+        $provider = $this->createMock(AbstractMilestoneAwareProvider::class);
 
-        $this->event->discoveredProvider($provider)->shouldBeCalled();
+        $this->providerSpec->expects($this->once())->method('isComplete')->willReturn(true);
+        $this->providerSpec->expects($this->once())->method('createProvider')->willReturn($provider);
+        $this->event->expects($this->never())->method('providerIsIncomplete');
+        $this->event->expects($this->once())->method('discoveredProvider')->with($provider);
 
-        $this->assertNull($this->listener->__invoke($this->event->reveal()));
+        $this->assertNull($this->listener->__invoke($this->event));
     }
 }
