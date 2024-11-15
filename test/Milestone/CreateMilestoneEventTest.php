1b1a7e931920f53367c03c0bd240b86730d8f10a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-15 09:22:11 -0600

    refactor: use native PHPUnit mocks with CreateMilestoneEventTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Milestone/CreateMilestoneEventTest.php
+++ test/Milestone/CreateMilestoneEventTest.php
@@ -10,10 +10,8 @@ namespace PhlyTest\KeepAChangelog\Milestone;
 
 use Phly\KeepAChangelog\Milestone\CreateMilestoneEvent;
 use Phly\KeepAChangelog\Provider\Milestone;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
@@ -21,34 +19,23 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateMilestoneEventTest extends TestCase
 {
-    use ProphecyTrait;
-
-    /** @var EventDispatcherInterface|ObjectProphecy */
-    private $dispatcher;
-
-    /** @var CreateMilestoneEvent */
-    private $event;
-
-    /** @var InputInterface|ObjectProphecy */
-    private $input;
-
-    /** @var OutputInterface|ObjectProphecy */
-    private $output;
+    private EventDispatcherInterface&MockObject $dispatcher;
+    private CreateMilestoneEvent $event;
+    private InputInterface&MockObject $input;
+    private OutputInterface&MockObject $output;
 
     public function setUp(): void
     {
-        $this->input = $this->prophesize(InputInterface::class);
-        $this->input->getArgument('title')->willReturn('2.0.0');
-        $this->input->getArgument('description')->willReturn('2.0.0 requirements');
+        $this->input      = $this->createMock(InputInterface::class);
+        $this->output     = $this->createMock(OutputInterface::class);
+        $this->dispatcher = $this->createMock(EventDispatcherInterface::class);
 
-        $this->output     = $this->prophesize(OutputInterface::class);
-        $this->dispatcher = $this->prophesize(EventDispatcherInterface::class);
+        $this->input->expects($this->any())->method('getArgument')->will($this->returnValueMap([
+            ['title', '2.0.0'],
+            ['description', '2.0.0 requirements'],
+        ]));
 
-        $this->event = new CreateMilestoneEvent(
-            $this->input->reveal(),
-            $this->output->reveal(),
-            $this->dispatcher->reveal()
-        );
+        $this->event = new CreateMilestoneEvent($this->input, $this->output, $this->dispatcher);
     }
 
     public function testUsesTitleArgumentFromInput(): void
@@ -64,8 +51,10 @@ class CreateMilestoneEventTest extends TestCase
     public function testMarkingMilestoneCreatedSendsOutput(): void
     {
         $this->output
-            ->writeln(Argument::containingString('Created milestone (1234) 2.0.0: 2.0.0 requirements'))
-            ->shouldBeCalled();
+            ->expects($this->once())
+            ->method('writeln')
+            ->with($this->stringContains('Created milestone (1234) 2.0.0: 2.0.0 requirements'));
+
         $milestone = new Milestone(1234, '2.0.0', '2.0.0 requirements');
 
         $this->assertNull($this->event->milestoneCreated($milestone));
@@ -76,25 +65,20 @@ class CreateMilestoneEventTest extends TestCase
     {
         $e = new RuntimeException('this is the error message');
 
-        $output = $this->output;
-        $output
-            ->writeln(Argument::containingString('Error creating milestone'))
-            ->will(function () use ($output) {
-                $output
-                    ->writeln(Argument::containingString('An error occurred when attempting to create the milestone'))
-                    ->will(function () use ($output) {
-                        $output
-                            ->writeln('')
-                            ->will(function () use ($output) {
-                                $output
-                                    ->writeln(Argument::containingString('Error Message: this is the error message'))
-                                    ->shouldBeCalled();
-                            })
-                            ->shouldBeCalled();
-                    })
-                    ->shouldBeCalled();
-            })
-            ->shouldBeCalled();
+        $invokedCount = $this->atLeast(4);
+        $this->output
+            ->expects($invokedCount)
+            ->method('writeln')
+            ->with($this->callback(function (string $message) use ($invokedCount): bool {
+                match ($invokedCount->getInvocationCount()) {
+                    1       => TestCase::assertStringContainsString('Error creating milestone', $message),
+                    2       => TestCase::assertStringContainsString('An error occurred when attempting to create the milestone', $message),
+                    3       => TestCase::assertSame('', $message),
+                    4       => TestCase::assertStringContainsString('Error Message: this is the error message', $message),
+                    default => true,
+                };
+                return true;
+            }));
 
         $this->assertNull($this->event->errorCreatingMilestone($e));
         $this->assertTrue($this->event->failed());
@@ -104,17 +88,18 @@ class CreateMilestoneEventTest extends TestCase
     {
         $e = new RuntimeException('this is the error message', 401);
 
-        $output = $this->output;
-        $output
-            ->writeln(Argument::containingString('Invalid credentials'))
-            ->will(function () use ($output) {
-                $output
-                    ->writeln(Argument::containingString(
-                        'The credentials associated with your Git provider are invalid'
-                    ))
-                    ->shouldBeCalled();
-            })
-            ->shouldBeCalled();
+        $invokedCount = $this->atLeast(2);
+        $this->output
+            ->expects($invokedCount)
+            ->method('writeln')
+            ->with($this->callback(function (string $message) use ($invokedCount): bool {
+                match ($invokedCount->getInvocationCount()) {
+                    1       => TestCase::assertStringContainsString('Invalid credentials', $message),
+                    2       => TestCase::assertStringContainsString('The credentials associated with your Git provider are invalid', $message),
+                    default => true,
+                };
+                return true;
+            }));
 
         $this->assertNull($this->event->errorCreatingMilestone($e));
         $this->assertTrue($this->event->failed());
