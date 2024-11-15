43edff4c4c3b76cfc76eeaa4f2f22378fe8eff50
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-15 12:04:38 -0600

    refactor: use native PHPUnit mocks with CreateReleaseNameListenerTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Version/CreateReleaseNameListenerTest.php
+++ test/Version/CreateReleaseNameListenerTest.php
@@ -11,45 +11,47 @@ namespace PhlyTest\KeepAChangelog\Version;
 use Phly\KeepAChangelog\Config;
 use Phly\KeepAChangelog\Version\CreateReleaseNameListener;
 use Phly\KeepAChangelog\Version\ReleaseEvent;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Symfony\Component\Console\Input\InputInterface;
 
 class CreateReleaseNameListenerTest extends TestCase
 {
-    use ProphecyTrait;
+    private InputInterface&MockObject $input;
+    private Config&MockObject $config;
+    private ReleaseEvent&MockObject $event;
 
     protected function setUp(): void
     {
-        $this->input  = $this->prophesize(InputInterface::class);
-        $this->config = $this->prophesize(Config::class);
-        $this->event  = $this->prophesize(ReleaseEvent::class);
-        $this->event->input()->will([$this->input, 'reveal']);
-        $this->event->config()->will([$this->config, 'reveal']);
+        $this->input  = $this->createMock(InputInterface::class);
+        $this->config = $this->createMock(Config::class);
+        $this->event  = $this->createMock(ReleaseEvent::class);
+
+        $this->event->expects($this->any())->method('input')->willReturn($this->input);
+        $this->event->expects($this->any())->method('config')->willReturn($this->config);
     }
 
     public function testSetsReleaseNameFromInputOptionWhenPresent()
     {
-        $this->input->getOption('name')->willReturn('some/package 1.2.3');
-        $this->event->setReleaseName('some/package 1.2.3')->shouldBeCalled();
+        $this->config->expects($this->never())->method('package');
+        $this->event->expects($this->once())->method('setReleaseName')->with('some/package 1.2.3');
+        $this->event->expects($this->never())->method('version');
+        $this->input->expects($this->once())->method('getOption')->with('name')->willReturn('some/package 1.2.3');
 
         $listener = new CreateReleaseNameListener();
 
-        $this->assertNull($listener($this->event->reveal()));
-
-        $this->config->package()->shouldNotHaveBeenCalled();
-        $this->event->version()->shouldNotHaveBeenCalled();
+        $this->assertNull($listener($this->event));
     }
 
     public function testSetsReleaseNameBasedOnPackageAndVersionWhenNoInputOptionPresent()
     {
-        $this->input->getOption('name')->willReturn(null);
-        $this->config->package()->willReturn('some/package');
-        $this->event->version()->willReturn('1.2.3');
-        $this->event->setReleaseName('package 1.2.3')->shouldBeCalled();
+        $this->config->expects($this->once())->method('package')->willReturn('some/package');
+        $this->event->expects($this->once())->method('setReleaseName')->with('package 1.2.3');
+        $this->event->expects($this->once())->method('version')->willReturn('1.2.3');
+        $this->input->expects($this->once())->method('getOption')->with('name')->willReturn(null);
 
         $listener = new CreateReleaseNameListener();
 
-        $this->assertNull($listener($this->event->reveal()));
+        $this->assertNull($listener($this->event));
     }
 }
