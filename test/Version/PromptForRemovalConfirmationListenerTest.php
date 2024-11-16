eed8a3b2a26d2f3fe0310473d89f13fbffa276a5
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-16 09:18:17 -0600

    refactor: use native PHPUnit mocks for PromptForRemovalConfirmationListenerTest
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/Version/PromptForRemovalConfirmationListenerTest.php
+++ test/Version/PromptForRemovalConfirmationListenerTest.php
@@ -11,9 +11,8 @@ namespace PhlyTest\KeepAChangelog\Version;
 use Phly\KeepAChangelog\Common\ChangelogEntry;
 use Phly\KeepAChangelog\Version\PromptForRemovalConfirmationListener;
 use Phly\KeepAChangelog\Version\RemoveChangelogVersionEvent;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -21,81 +20,77 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class PromptForRemovalConfirmationListenerTest extends TestCase
 {
-    use ProphecyTrait;
+    private ChangelogEntry $entry;
+    private InputInterface&MockObject $input;
+    private OutputInterface&MockObject $output;
+    private QuestionHelper&MockObject $helper;
+    private RemoveChangelogVersionEvent&MockObject $event;
+    private PromptForRemovalConfirmationListener $listener;
 
     protected function setUp(): void
     {
         $this->entry  = new ChangelogEntry();
-        $this->input  = $this->prophesize(InputInterface::class);
-        $this->output = $this->prophesize(OutputInterface::class);
-        $this->helper = $this->prophesize(QuestionHelper::class);
-        $this->event  = $this->prophesize(RemoveChangelogVersionEvent::class);
+        $this->input  = $this->createMock(InputInterface::class);
+        $this->output = $this->createMock(OutputInterface::class);
+        $this->helper = $this->createMock(QuestionHelper::class);
+        $this->event  = $this->createMock(RemoveChangelogVersionEvent::class);
 
         $this->entry->contents = 'Changelog contents';
 
-        $this->input->hasOption(Argument::type('string'))->willReturn(null);
-        $this->input->getOption(Argument::type('string'))->willReturn(null);
-
-        $this->output->writeln(Argument::type('string'))->willReturn(null);
-
-        $this->event->input()->will([$this->input, 'reveal']);
-        $this->event->output()->will([$this->output, 'reveal']);
-        $this->event->changelogEntry()->willReturn($this->entry);
-        $this->event->abort()->will(function () {
-        });
+        $this->event->expects($this->any())->method('input')->willReturn($this->input);
+        $this->event->expects($this->any())->method('output')->willReturn($this->output);
+        $this->event->expects($this->any())->method('changelogEntry')->willReturn($this->entry);
 
         $this->listener                 = new PromptForRemovalConfirmationListener();
-        $this->listener->questionHelper = $this->helper->reveal();
+        $this->listener->questionHelper = $this->helper;
     }
 
     public function testListenerReturnsEarlyIfForceRemovalOptionToggledOn()
     {
-        $this->input->hasOption('force-removal')->willReturn(true);
-        $this->input->getOption('force-removal')->willReturn(true);
+        $this->input->expects($this->once())->method('hasOption')->with('force-removal')->willReturn(true);
+        $this->input->expects($this->once())->method('getOption')->with('force-removal')->willReturn(true);
 
-        $this->assertNull(($this->listener)($this->event->reveal()));
+        $this->event->expects($this->atLeastOnce())->method('input');
+        $this->event->expects($this->never())->method('output');
+        $this->event->expects($this->never())->method('changelogEntry');
+        $this->event->expects($this->never())->method('abort');
 
-        $this->event->input()->shouldHaveBeenCalled();
-        $this->event->changelogEntry()->shouldNotHaveBeenCalled();
-        $this->event->output()->shouldNotHaveBeenCalled();
-        $this->event->abort()->shouldNotHaveBeenCalled();
+        $this->assertNull(($this->listener)($this->event));
     }
 
     public function testListenerAbortsEventOnUserRequest()
     {
-        $this->input->hasOption('force-removal')->willReturn(false);
+        $this->input->expects($this->once())->method('hasOption')->with('force-removal')->willReturn(false);
+
+        $this->event->expects($this->atLeastOnce())->method('input');
+        $this->event->expects($this->atLeastOnce())->method('changelogEntry');
+        $this->event->expects($this->atLeastOnce())->method('output');
+        $this->event->expects($this->atLeastOnce())->method('abort');
+
         $this->helper
-            ->ask(
-                Argument::that([$this->input, 'reveal']),
-                Argument::that([$this->output, 'reveal']),
-                Argument::type(ConfirmationQuestion::class)
-            )
+            ->expects($this->once())
+            ->method('ask')
+            ->with($this->input, $this->output, $this->isInstanceOf(ConfirmationQuestion::class))
             ->willReturn(false);
 
-        $this->assertNull(($this->listener)($this->event->reveal()));
-
-        $this->event->input()->shouldHaveBeenCalled();
-        $this->event->changelogEntry()->shouldHaveBeenCalled();
-        $this->event->output()->shouldHaveBeenCalled();
-        $this->event->abort()->shouldHaveBeenCalled();
+        $this->assertNull(($this->listener)($this->event));
     }
 
     public function testListenerDoesNotNotifyEventIfUserDoesNotAbort()
     {
-        $this->input->hasOption('force-removal')->willReturn(false);
+        $this->input->expects($this->once())->method('hasOption')->with('force-removal')->willReturn(false);
+
+        $this->event->expects($this->atLeastOnce())->method('input');
+        $this->event->expects($this->atLeastOnce())->method('changelogEntry');
+        $this->event->expects($this->atLeastOnce())->method('output');
+        $this->event->expects($this->never())->method('abort');
+
         $this->helper
-            ->ask(
-                Argument::that([$this->input, 'reveal']),
-                Argument::that([$this->output, 'reveal']),
-                Argument::type(ConfirmationQuestion::class)
-            )
+            ->expects($this->once())
+            ->method('ask')
+            ->with($this->input, $this->output, $this->isInstanceOf(ConfirmationQuestion::class))
             ->willReturn(true);
 
-        $this->assertNull(($this->listener)($this->event->reveal()));
-
-        $this->event->input()->shouldHaveBeenCalled();
-        $this->event->changelogEntry()->shouldHaveBeenCalled();
-        $this->event->output()->shouldHaveBeenCalled();
-        $this->event->abort()->shouldNotHaveBeenCalled();
+        $this->assertNull(($this->listener)($this->event));
     }
 }
