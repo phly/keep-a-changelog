ea87d7a396d3e35a14d381e089729a255b0fb71a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2024-11-17 12:55:04 -0600

    fix: fix the scoper configuration
    Since we last built, php-scoper has a new pre-1.0 release, 0.18, which removes "whitelist" verbiage in favor of "expose"... which made our previous configuration break.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,16 +2,14 @@
 
 declare(strict_types=1);
 
-use Isolated\Symfony\Component\Finder\Finder;
-
 return [
-    'whitelist' => [
-        // Whitelist providers
+    'expose-classes' => [
+        // Expose providers and provider interfaces
         'Phly\KeepAChangelog\Provider\GitHub',
         'Phly\KeepAChangelog\Provider\GitLab',
         'Phly\KeepAChangelog\Provider\MilestoneAwareProviderInterface',
         'Phly\KeepAChangelog\Provider\ProviderInterface',
     ],
     // Necessary for allowing polyfill classes
-    'whitelist-global-classes' => false,
+    'expose-global-classes' => false,
 ];
